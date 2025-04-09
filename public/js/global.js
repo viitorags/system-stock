cda222b350bb3a8ad3755da49e8bc1eb7742758b
viitorags <vitorgabriels1000@gmail.com>
2025-04-09 20:07:56 -0300

    Adicionando javascript para fechar e abrir o menu e removendo hidden do index.css

--- public/js/global.js
+++ public/js/global.js
@@ -1,2 +1,11 @@
+const menuButton = document.querySelector(".menu_toggle");
+const menu = document.querySelector(".sidebar_menu");
+
 // Caminho dos links do menu
 
+
+// Abrir e fechar menu no Mobile
+
+menuButton.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
