7757efc46a2c86079129459be46f2e7f707a744e
viitorags <vitorgabriels1000@gmail.com>
2025-05-13 19:28:41 -0300

    Adding db config

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import pg from "pg";
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+export default pool;
